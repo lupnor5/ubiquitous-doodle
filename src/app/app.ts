e3be46cbe399d6318df41a5bd0b1801636d846b1
Maria Guadalupe Ramirez Cruz <lupnor5@gmail.com>
2026-02-19 22:12:58 -0600

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-angular-app');
